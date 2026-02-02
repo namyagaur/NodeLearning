81fb80d6c939cc2f20afd787c9839866ee8e398c
namyagaur <namyaagaur@gmail.com>
2026-02-02 12:33:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+//Module - require se import krte hai node js mein
+//repl se cmd se krskte h cocde
+
+fs.writeFileSync('notes.txt', 'This file was created by Node.js');
+// function fruit(item){
+//     console.log(item);
+// }
+// fruit("apple"); 
+// fruit("banana");
+// fruit("cherry");
+
+// var a = 10;
+// var b = 20;
+// console.log(a + b);
