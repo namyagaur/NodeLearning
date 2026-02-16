795909a547821bad2056a1e1d524aec14a6fd7ea
namyagaur <namyaagaur@gmail.com>
2026-02-16 10:47:00 +0530

    Made my first server

---
+++ colors.js
@@ -0,0 +1,4 @@
+const colors = require('colors');
+
+console.log(colors.rainbow('This text is red'));
+console.log(colors.blue('This text is red'));
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
 http.createServer((req,resp)=>{
-resp.write("<h1>Hello This is Namya Gaur</h1>");
+resp.write("<h1>Hello, This is Namya Gaur</h1><h3>Welcome to my Node.js server</h3>");
 resp.end();
 }).listen(4800);
+
+http.createServer((req,resp)=>{
+resp.write("<h1>Hello, other server</h1><h3>Welcome to my Node.js server</h3>");
+resp.end();
+}).listen(5800);
+
