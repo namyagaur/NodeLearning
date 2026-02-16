69c059754a92a1cea84428ec20c1ccac16e90600
namyagaur <namyaagaur@gmail.com>
2026-02-16 10:49:53 +0530

    nothing

--- basics.js
+++
@@ -1,8 +0,0 @@
-// import {username} from "./data.js";
-// //ye cheez node mein ni chalti hai, ye ES6 module ka syntax hai, node mein commonjs module ka syntax use hota hai
-// //node mein import karne ke liye require ka use hota hai, aur export karne ke liye module.exports ka use hota hai
-
-const {username} = require('./data.js'); 
-//ye commonjs module ka syntax hai, node mein use hota hai
-console.log(username); 
-//ye commonjs module ka syntax hai, node mein use hota hai
--- data.js
+++
@@ -1,4 +0,0 @@
-// export let username = "JohnDoe";
-module.exports ={
-    username: "Namya Gaur"
-}
